2c169209f4c4550f6501f4119b787d120197386d
Claudio Olmedo <claudio@centromaker.com>
2016-05-20 21:40:01 -0300

    13 Pin Out and time control by Arduino

--- serialArduino/serialArduino.ino
+++ serialArduino/serialArduino.ino
@@ -1,28 +1,30 @@
+//IGA/DESLIGA LED
 
-const int PIN = 8;
-int incomingByte = 0;
+/*
+ * comandos via serial
+ * inverte o estado do led conctado a saída 13 do arduino quando recebe o caracter 'A' pela serial
+ */
+
+const int LED = 13;
+
 void setup() {
   Serial.begin(9600);    //configura comunicação serial com 9600 bps
-  pinMode(PIN,OUTPUT);   //configura pino do led como saída
+  pinMode(LED,OUTPUT);   //configura pino do led como saída
 }
 
 void loop() {
-
-        // send data only when you receive data:
-        if (Serial.available() > 0) {
-          
-                incomingByte = Serial.read();
-
-                
-                if (incomingByte=='A') {
-                  
-                    digitalWrite(PIN,HIGH); 
-                    delay(1000);
-                    digitalWrite(PIN,LOW);
-                }
-                
-        
+   if (Serial.available()) //se byte pronto para leitura
+   {
+    
+    switch(Serial.read())      //verifica qual caracter recebido
+    {
+      case 'A':                  //caso 'A'
+      
+        digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);              // wait for a second
+        digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+        delay(1000);  
+      break;
     }
   }
-
-
+}
